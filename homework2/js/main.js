e4abcb39a3b2afb1cf88ee97d5083517ac6fcceb
senya10 <arsent1310@mail.ru>
2019-03-27 16:54:35 +0300

    hw3 new

--- homework2/js/main.js
+++ homework2/js/main.js
@@ -1,13 +1,13 @@
 let a = 0 || 'string';
-console.log(a);//первое значение, которое является true
+console.log(a); //первое значение, которое является true
 
-let b = 1 && 'string'; //Первый аргумент - true, возвращается второй аргумент
+let b = 1 && 'string'; //Левый аргумент - true, возвращается правый аргумент,который тоже true
 console.log(b);
 
-let c = null || 25;//25 - первое значение, которое является true
+let c = null || 25; //25 - первое значение, которое является true
 console.log(c);
 
-let d = null && 25;//null, 1е значение false возвращается, 2ой аргумент игнорир
+let d = null && 25; //null, 1е значение false возвращается, 2ой аргумент игнорир
 console.log(d);
 
 let e = null || 0 || 35; //35 первое значение, которое является true
@@ -22,21 +22,23 @@ console.log(g);
 
 '3' + 2 - 1 // 31, т к используется бинарный + делающий второй аргумент (результат вычисл 2-1) строчным
 
-true + 2 //3 т к true становится числом 1
+true + 2 //3 т к true при числовом переводе становится числом 1
 
 +'10' + 1 //11 т к перед '10' используется унарный +, превращающий строчное значение в числовое
 
 undefined + 2 //NaN , undefined при числовом преобразовании NaN
 
-null + 5    // null ри числовом преобразовании становится 0
+null + 5    //5, null при числовом преобразовании становится 0
 
 true + undefined    //NaN
+
 
 let x = 'hidden';               //Если переменная равна “hidden”, присвоить ей значение “visible”, иначе - “hidden”.
 if (x ==='hidden'){
     console.log(x = 'visible');
 }else{
     console.log(x);}
+
 
 let n = 0;
 if (n===0){
@@ -47,6 +49,7 @@ if (n===0){
     console.log(n*=10);         //если больше нуля - используя оператор “присвоение”, переменную умножить на 10
 }
 
+
 let car = { name: 'Lexus', age: 10, create: 2008, needRepair: false }
 if (car.age > 5){
     console.log('Need Repair');  // если возраст машины больше 5 лет то нужно вывести в консоль сообщение 'Need Repair'
@@ -55,24 +58,27 @@ if (car.age > 5){
     car.needRepair = false;      // иначе изменить на false
 }
 
-let item = { name: 'Intel core i7', price: '100$', discount: '15%' }
-
-
-
+// Задача №4
+let item = { name: 'Intel core i7', price: '174$', discount: '17$'}
+if (item.discount &&(item.discount !== '' || item.discount !== null || item.discount !== undefined)){
+    item.priceWithDiscount = ((100-parseInt(item.discount,10)) / 100) * parseInt(item.price,10);
+    console.log(item.priceWithDiscount + '$');
+}else{
+    console.log(item.price);
+}
 
 
 let product = {
     name: "Яблоко",
     price: "20$"
 }
-
 let min = 10;   //Написать условие если цена товара больше или равна минимальной цене и меньше или равна максимальной цене то вывести в консоль название этого товара, иначе вывести в консоль что товаров не найдено.
 let max = 20;
-product.price = parseInt(product.price,10);
-if (product.price >= min && product.price <= max){
+if (parseInt(product.price,10) >= min && parseInt(product.price,10) <= max){
     console.log(product.name);
 }else{
     console.log('товаров не найдено');}
+
 
 let mobile = {product: 'iphone'};             // Создать объект с полем product, равным ‘iphone’
 mobile.price = 1000;                        // Добавить в объект поле price, равное 1000 и поле currency, равное ‘dollar’

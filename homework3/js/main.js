031c08c3df42194b792b5c8065785cd352f8b0c0
senya10 <arsent1310@mail.ru>
2019-03-30 15:38:24 +0300

    my work

---
+++ homework3/js/main.js
@@ -0,0 +1,112 @@
+//part1
+//#1
+
+let a = 'none';
+a === 'block' 
+    ? console.log('block')
+    : a === 'none' 
+        ? console.log('none')
+        : a === 'inline' 
+            ? console.log('inline')
+            : console.log('other')
+ 
+let x = '';
+switch(x){
+    case 'block':
+        console.log('block');
+        break
+    case 'none':
+        console.log('none');
+        break
+    case 'inline':
+        console.log('inline');
+        break
+    default: console.log('other');
+}
+//#2
+
+let f = '';
+f === 'hidden'
+    ? [f = 'visible', console.log('visible')]
+    : [f = 'hidden', console.log('hidden')];
+
+let p = '3';
+p === 0
+    ? [p = 1, console.log(p = 1)]
+    : p < 0
+        ? [p = 'less then zero', console.log(p = 'less then zero')]
+        : p > 0
+            ? [p*=10, console.log(p*= 10)]
+            : console.log(p);
+
+//#3
+let car = { name: 'Lexus', age: 6, create: 2008, needRepair: false }
+car.age > 5
+    ? [console.log('Need Repair'), car.needRepair = true]
+    : car.needRepair = false;
+
+//part2
+//#1
+
+let g = 'i am in the easycode'
+let h = '';
+
+for (let i = 0; i < g.length; i++){
+    h += (g [i-1] == ' ')
+    ? g[i].toUpperCase() 
+    : g[i];
+}
+    console.log(h);
+
+//#2
+
+let c = 'tseb eht ma i';
+let d = '';
+ 
+for(let i = c.length-1; i >=0; i--){
+    d += c[i];
+}
+console.log(d);
+
+//#3
+
+function factorial(n){
+    let result = 1;
+    while (n--){
+        result *= n+1
+    }
+    return result;
+}
+console.log(factorial(10));
+
+//4#
+
+for (a='JavaScript is a pretty good language', b = '', i=0; i < a.length; i++){
+    b += ' ' != a[i] 
+    ? i && ' ' != a[i-1] 
+    ? a[i] 
+    : a[i].toUpperCase() : '';
+}
+console.log(b);
+
+//5#
+
+for (let i = 1; i <= 15; i++){
+    if (i%2 === 1){
+        console.log(i);
+    }
+}
+
+//#6
+
+let list = {
+    name: 'denis',
+    work: 'easycode',
+    age: 29
+   }
+
+for(let prop in list) {
+    if (typeof(list[prop]) === 'string')
+    list[prop] = list[prop].toUpperCase();
+    console.log(list[prop]);
+}
